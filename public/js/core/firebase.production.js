641e4e8e05547b1f9451fbb0214216502f8a681f
Edson Silva <seuemail@gmail.com>
2026-02-20 12:26:56 -0300

    fix: corrigindo firebase production config

--- public/js/core/firebase.production.js
+++ public/js/core/firebase.production.js
@@ -1,9 +1,11 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AlzaSyBh6CIne05dCu0Omu7JX6icZv8l7c2bw_8",
+  apiKey: "AIzaSyBh6CIne05dCuO0mu7JX6icZv8l7c2bw_8",
   authDomain: "meu-app-edson.firebaseapp.com",
   projectId: "meu-app-edson",
   storageBucket: "meu-app-edson.firebasestorage.app",
@@ -11,8 +13,5 @@ const firebaseConfig = {
   appId: "1:555388653411:web:e9184f7f5e443174934d56"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { app, auth, db };
